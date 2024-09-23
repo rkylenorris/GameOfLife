1c2f286ecb3e6c7c67effe63706c5d7e7e3f75e6
Kyle Norris <r.kyle.norris@gmail.com>
2024-09-23 18:11:41 -0400

    added skeleton code and updated main.py to include project source url

---
+++ main.py
@@ -0,0 +1,10 @@
+# Author: R. Kyle Norris
+# capstone project for https://learn.365datascience.com/ Python Bootcamp course
+# found at https://learn.365datascience.com/projects/building-conway-s-game-of-life-in-python/
+# code provided is in skeleton_code folder
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
